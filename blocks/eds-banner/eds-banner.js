68e56e415a7c80fde2bc41c9e4e51376954df500
wetzka <wetzka@adobe.com>
2024-05-17 09:10:57 +0200

    added banner block

---
+++ blocks/eds-banner/eds-banner.js
@@ -0,0 +1,17 @@
+export default function decorate(block) {
+  const cols = [...block.firstElementChild.children];
+
+  // setup image columns
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((col) => {
+      const pic = col.querySelector('picture');
+      if (pic) {
+        const picWrapper = pic.closest('div');
+        if (picWrapper && picWrapper.children.length === 1) {
+          // picture is only content in banner
+          picWrapper.classList.add('eds-banner-img');
+        }
+      }
+    });
+  });
+}
