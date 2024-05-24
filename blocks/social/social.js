880ae3c35e532def1a1994582342ce724e6f19f2
wetzka <wetzka@adobe.com>
2024-05-24 11:57:30 +0200

    added social-block

---
+++ blocks/social/social.js
@@ -0,0 +1,15 @@
+export default function decorate(block) {
+    // setup image columns
+    [...block.children].forEach((row) => {
+      [...row.children].forEach((col) => {
+        const pic = col.querySelector('picture');
+        if (pic) {
+          const picWrapper = pic.closest('div');
+          if (picWrapper && picWrapper.children.length === 1) {
+            picWrapper.classList.add('social-img');
+          }
+        }
+      });
+    });
+  }
+  
