f7ba480892c2f80877132548523dea259757a4d0
wetzka <wetzka@adobe.com>
2024-06-11 17:19:38 +0200

    fixed reported issues

--- blocks/magazine-list/magazine-list.js
+++ blocks/magazine-list/magazine-list.js
@@ -1,16 +1,14 @@
 export default async function decorate(block) {
   const response = await fetch('/query-index.json');
   const data = await response.json();
-  data.data.forEach(magazine => {
+  data.data.forEach( (magazine ) => {
     if (magazine.path.startsWith('/us/en/magazine/')) {
       const title = document.createElement('h3');
       title.textContent = magazine.title;
-      block.append(title);
-      
+      block.append(title);      
       const description = document.createElement('p');
       description.textContent = magazine.description;
-      block.append(description);
-      
+      block.append(description);      
       const link = document.createElement('a');
       const linkText = document.createTextNode('Read more');
       link.appendChild(linkText);
